927e00f89806bb9d4850ab07110bf9f21326ee90
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-23 20:05:39 -0300

    Faz requisição à API do GitHub via axios

---
+++ axios/main.js
@@ -0,0 +1,11 @@
+var url = 'https://api.github.com/users/gmonteeeiro';
+
+axios.get(url)
+    .then(function(response)
+    {
+        console.log(response);
+    })
+    .catch(function(error)
+    {
+        console.warn(error);
+    })
