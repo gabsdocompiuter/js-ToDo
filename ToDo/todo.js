c8d9e4b9c3374fd7cd6a15949bcaf2a7f93f969d
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-23 17:14:03 -0300

    Adiciona método para remover itens da lista

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -12,18 +12,29 @@ var itens =
 function CarregaItens()
 {
     listaElement.innerHTML = '';
-    
+
     for(item of itens)
     {
         var itemElement = document.createElement('li');
         var itemText = document.createTextNode(item);
-
         itemElement.appendChild(itemText);
+        
+        var excluirElement = document.createElement('a');
+        var excluirText = document.createTextNode('Excluir');
+        excluirElement.setAttribute('href', '#');
+
+        pos = itens.indexOf(item);
+        excluirElement.setAttribute('onclick',  'RemoveItem(' + pos + ')')
+
+        excluirElement.appendChild(excluirText);
+        itemElement.appendChild(excluirElement);
+
+
         listaElement.appendChild(itemElement);
     }
 }
 
-function AddTodo()
+function AddItem()
 {
     var novoItem = inputElement.value;
     itens.push(novoItem);
@@ -33,8 +44,16 @@ function AddTodo()
     CarregaItens();
 }
 
-buttonElement.onclick = function(){
-    AddTodo();
+function RemoveItem(pos)
+{
+    itens.splice(pos, 1);
+
+    CarregaItens();
+}
+
+buttonElement.onclick = function()
+{
+    AddItem();
 }
 
 
